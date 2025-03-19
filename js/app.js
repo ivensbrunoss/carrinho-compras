f42e9a78df5a2e51b75c426219c86a3fa8d22740
Ivens Santos <ivens_bruno@hotmail.com>
2025-03-19 13:14:03 -0300

    adicionando validacao de quantidade

--- js/app.js
+++ js/app.js
@@ -2,28 +2,33 @@ let totalGeral = 0;
 limpar();
 
 function adicionar() {
-    //recuperar nome do produto, valor e quantidade
+    //recuperar informações do produto
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
 
-    // calcular subtotal dos produtos
+    //recuperar quantidade
     let quantidade = document.getElementById('quantidade').value;
-    let preco = valorUnitario * quantidade;
 
-    //adicionar o produto na área do carrinho
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-        </section>`
+    //validação
+    if (quantidade > 0) {
+        //adicionar o produto na área do carrinho
+        let preco = valorUnitario * quantidade;
+        let carrinho = document.getElementById('lista-produtos');
+        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+              <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+            </section>`
 
-    //calcular valor total do carrinho
-    totalGeral = totalGeral + preco;
-    let campoValorTotal = document.getElementById('valor-total');
-    campoValorTotal.textContent = `R$ ${totalGeral}`;
+        //calcular valor total do carrinho
+        totalGeral = totalGeral + preco;
+        let campoValorTotal = document.getElementById('valor-total');
+        campoValorTotal.textContent = `R$ ${totalGeral}`;
 
-    //zerar quantidade
-    document.getElementById('quantidade').value = 0;
+        //zerar quantidade
+        document.getElementById('quantidade').value = 0;
+    } else {
+        alert('Você não adicionou uma quantidade válida');
+    }
 }
 
 function limpar() {
